0a65195bd66b1643c42d2dba1c55a064bb1a2a8c
Oleg Ivanov <ivanov@megaputer.ru>
2018-07-10 14:12:52 +0300

    Session class has been added (login available)
    gemspec (and Gemfile.lock)
    - rest-client gem is added
    
    polyanalyst6api.rb have got needed 'require' invocations
    
    Session class with constructor (login proceudre) is added

--- lib/polyanalyst6api.rb
+++ lib/polyanalyst6api.rb
@@ -1,5 +1,9 @@
 require 'polyanalyst6api/version'
 
+require 'rest-client'
+
+require 'polyanalyst6api/session'
+
 module PolyAnalyst6API
   # Your code goes here...
 end
---
+++ lib/polyanalyst6api/session.rb
@@ -0,0 +1,18 @@
+module PolyAnalyst6API
+  class Session
+    attr_reader :sid
+
+    def initialize(domen: 'localhost', port: 5043, v: '1.0', uname: 'administrator', pwd: '')
+      @base_url = "https://#{domen}:#{port}/polyanalyst/api/v#{v}"
+      login_url = @base_url + "/login?uname=#{uname}&pwd=#{pwd}"
+      params = {
+        method: :post,
+        url: login_url,
+        verify_ssl: false
+      }
+      resp = RestClient::Request.execute params
+      @sid = resp.cookies['sid']
+      raise 'Login failed!' unless @sid
+    end
+  end
+end
